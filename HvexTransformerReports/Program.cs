c82ae6b35986a7292d171523b5a725af40c3af18
Thiago Mira <tbmira_prog@outlook.com>
2022-12-17 18:41:32 -0300

    Configurar conexão com banco de dados

--- HvexTransformerReports/Program.cs
+++ HvexTransformerReports/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("DatabaseSettings"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
